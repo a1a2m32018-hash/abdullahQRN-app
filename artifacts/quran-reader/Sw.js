210252a29d1e15d964938bfae44a61eb5018f858
a1a2m32018-hash <a1a2m32018@gmail.com>
2026-09-03 22:50:48 +0300

    Create Sw.js

---
+++ artifacts/quran-reader/Sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'quran-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/src/main.tsx'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
